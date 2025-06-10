afdbe545a3bf4fb0785d733ffc296b64dbdb23f9
Tommasomerola <merolatommaso77@gmail.com>
2025-06-10 15:15:44 +0200

    snack 4

--- exArray.js
+++ exArray.js
@@ -107,3 +107,18 @@ if(areAuthorsAdults){
 
 
 console.log(authors);
+
+
+/* Snack 3 - Calcola l’età media
+Creare un array (ages) che contiene le età degli autori dei libri.
+Calcola la somma delle età (agesSum) usando reduce.
+Stampa in console l’età media degli autori dei libri. */
+
+const ages = books.map(b => b.author.age);
+
+const agesSum = ages.reduce ((acc, age) => acc + age, 0)
+
+
+
+console.log(`L' età media degli autori è : ${agesSum / ages.length} anni`);
+
